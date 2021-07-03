6178fcb6f6707099631381bf92dd073cd7dce575
tcm <tylermckay10@gmail.com>
2021-07-03 16:20:31 -0300

    work done on the player movement and camera follow

--- Assets/Scripts/Player/FollowCamera.cs
+++ Assets/Scripts/Player/FollowCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace OGAM
@@ -5,28 +6,45 @@ namespace OGAM
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] private Transform target;
+
+        public float followDistance = 100f;
+        public float followSpeed = 2.5f;
         
         new private Camera camera;
 
         private Vector2 screenCenter;
         private Vector2 targetScreenPosition;
         private Vector2 distanceFromCenter;
+        private Vector2 velocity = Vector2.zero;
 
         private void Awake()
         {
             camera = GetComponent<Camera>();
-            
             screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
-            targetScreenPosition = camera.WorldToScreenPoint(target.position);
-            distanceFromCenter = screenCenter - targetScreenPosition;
         }
 
         private void Update()
         {
             targetScreenPosition = camera.WorldToScreenPoint(target.position);
             distanceFromCenter = screenCenter - targetScreenPosition;
-            
-            // if (distanceFromCenter.magnitude > )
         }
+
+        private void LateUpdate()
+        {
+            if (distanceFromCenter.magnitude > followDistance)
+            {
+                var targetPosition = target.position;
+                targetPosition.z = -10f;
+                transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+            }
+
+            // camera.transform.position = Vector2.SmoothDamp(camera.transform.position, target.position, ref velocity, followSpeed);
+        }
+
+        // private void OnDrawGizmos()
+        // {
+        //     Gizmos.color = Color.red;
+        //     
+        // }
     }
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -5,31 +6,58 @@ namespace OGAM.Player
 {
     public class Movement : MonoBehaviour
     {
+        //+ COMPONENTS
         new private Rigidbody2D rigidbody;
+
+        public LayerMask groundMask;
+        public float movementSpeed = 4f;
+        public float jumpHeight = 2.5f;
+
+        //+ LOCAL STATE
+        private Vector2 velocity;
         private bool jumping;
+        private bool grounded;
 
+        private const float GroundedDistance = 0.60f;
 
-        private void Awake()
-        {
-            rigidbody = GetComponent<Rigidbody2D>();
-        }
+        //> INITIALIZATION
+        private void Awake() => rigidbody = GetComponent<Rigidbody2D>();
 
+        //> HANDLE INPUT
         private void Update()
         {
+            // cache velocity for later
+            velocity = rigidbody.velocity;
+            
             jumping |= Input.GetKeyDown(KeyCode.Space);
+            velocity.x = movementSpeed * Input.GetAxis("Horizontal");
+        }
 
-            if (jumping)
+        //> HANDLE PHYSICS
+        private void FixedUpdate()
+        {
+            // check if player is grounded
+            var hit = Physics2D.Raycast(rigidbody.position, Vector2.down, GroundedDistance, groundMask);
+            grounded = hit.collider is { };
+            
+            
+            if (grounded && jumping)
             {
                 jumping = false;
-                rigidbody.velocity += 5f * Vector2.up;
+                velocity.y += Mathf.Sqrt(2f * Physics2D.gravity.magnitude * jumpHeight);
             }
             
-            var velocity = rigidbody.velocity;
-            velocity.x = 4f * Input.GetAxis("Horizontal");
+            // apply the desired velocity to the player
             rigidbody.velocity = velocity;
+        }
 
-
-
+        //> DRAW HELPFUL GIZMOS
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying) return;
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(rigidbody.position, Vector3.down * GroundedDistance);
         }
     }
 }
