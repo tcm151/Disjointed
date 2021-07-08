87620cb33e791fdd43b5d3970ba528301f25ae18
tcm <tylermckay10@gmail.com>
2021-07-08 12:57:56 -0300

    got wall jumps in an okay place

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -11,10 +11,14 @@ namespace OGAM.Player
         new private Rigidbody2D rigidbody;
 
         //- TWEAKABLES
-        public LayerMask jumpingMask;
-        public float movementSpeed = 4f;
+        [Header("Ground Checking")]
+        public LayerMask groundMask;
+        public float groundedDistance = 0.85f;
+        [Header("Movement")]
+        public float maxSpeed = 4f;
+        public float maxAcceleration = 10f;
+        [Header("Jumping")]
         public float jumpHeight = 2.5f;
-        public float jumpForce;
         public float jumpGravity = 1f;
         public float fallGravity = 2f;
 
@@ -23,47 +27,49 @@ namespace OGAM.Player
         private Vector2 contactNormal;
         private Vector2 desiredVelocity;
         private int timeSinceGrounded;
+        private int timeSinceContact;
         private bool jumping;
         private bool holdingJump;
-        private bool grounded;
-        private bool onWall;
+        public bool grounded;
+        public bool onWall;
+        public int contacts;
 
         //- CONSTANTS
-        public float GroundedDistance = 0.85f;
         private static readonly Vector3 FaceRight = new Vector3( 1, 1, 1);
         private static readonly Vector3 FaceLeft  = new Vector3(-1, 1, 1);
 
         //> INITIALIZATION
-        private void Awake() => rigidbody = GetComponent<Rigidbody2D>();
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
 
         //> HANDLE INPUT
         private void Update()
         {
-            // cache desiredVelocity for later
-            desiredVelocity = rigidbody.velocity;
-            
-            // get movement input
+            // get input
             jumping |= Input.GetKeyDown(KeyCode.Space);
             holdingJump = Input.GetKey(KeyCode.Space);
-            rigidbody.gravityScale = (holdingJump) ? jumpGravity : fallGravity;
-            
-            movementInput.x = movementSpeed * Input.GetAxis("Horizontal");
-            movementInput.y = movementSpeed * Input.GetAxis("Vertical");
+            movementInput.x = Input.GetAxisRaw("Horizontal");
+            movementInput.y = Input.GetAxisRaw("Vertical");
 
-            if (movementInput.x > 0) transform.localScale = FaceRight;
-            if (movementInput.x < 0) transform.localScale = FaceLeft;
-
-            //@ temporary would like to slow down the player instead of stopping immediately 
-            desiredVelocity.x = movementInput.x;
+            // update direction of sprite
+            if (rigidbody.velocity.x > 0) transform.localScale = FaceRight;
+            if (rigidbody.velocity.x < 0) transform.localScale = FaceLeft;
         }
 
         //> HANDLE PHYSICS
         private void FixedUpdate()
         {
-            timeSinceGrounded++; // give the player a jump buffer to jump
-            
-            // check if player is grounded
-            var hit = Physics2D.Raycast(rigidbody.position, Vector2.down, GroundedDistance, jumpingMask);
+            //+ UPDATE STATE
+            timeSinceContact++;
+            timeSinceGrounded++; // jump buffer
+            desiredVelocity = rigidbody.velocity; // cache current velocity
+            rigidbody.gravityScale = (holdingJump) ? jumpGravity : fallGravity; // apply more gravity on fall
+            if (timeSinceContact > 10) jumping = false;
+
+            //+ CHECK GROUNDED
+            var hit = Physics2D.Raycast(rigidbody.position, Vector2.down, groundedDistance, groundMask);
             if (hit.collider is { })
             {
                 grounded = true;
@@ -71,45 +77,53 @@ namespace OGAM.Player
             }
             else grounded = false;
 
+            //+ HORIZONTAL MOVEMENT
+            if (grounded || movementInput.x != 0)
+            {
+                var maxDeltaSpeed = maxAcceleration * Time.deltaTime;
+                desiredVelocity.x = Mathf.MoveTowards(desiredVelocity.x, movementInput.x * maxSpeed, maxDeltaSpeed);
+            }
+            
             // jump if the player is grounded & trying to jump
             if ((grounded || timeSinceGrounded < 5) && jumping)
             {
                 jumping = false;     // use this formula to get exact jump height
-                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                Debug.Log("JUMPED!");
+                desiredVelocity.y += Mathf.Sqrt(2f * Physics2D.gravity.magnitude * jumpHeight);
+                // Debug.Log("REGULAR JUMP!");
             }
 
-            if (onWall && jumping)
+            if ((onWall || timeSinceContact < 5) && jumping)
             {
                 jumping = false;
                 var jumpDirection = (contactNormal + Vector2.up).normalized;
-                rigidbody.AddForce(jumpDirection * jumpForce / 2f, ForceMode2D.Impulse);
-                Debug.Log("JUMPED!");
+                desiredVelocity += jumpDirection * Mathf.Sqrt(2f * Physics2D.gravity.magnitude * jumpHeight);
+                // Debug.Log("WALL JUMP!");
             }
+
+            desiredVelocity.y = Mathf.Clamp(desiredVelocity.y, float.MinValue, Mathf.Sqrt(2f * Physics2D.gravity.magnitude * jumpHeight));
 
             // assign the final velocity
             rigidbody.velocity = desiredVelocity;
         }
 
-        private void OnCollisionStay2D(Collision2D collision) => ManageCollisions(collision);
-        private void OnCollisionExit2D(Collision2D collision) => ManageCollisions(collision);
-
+         private void OnCollisionStay2D(Collision2D collision) => ManageCollisions(collision);
+         private void OnCollisionExit2D(Collision2D collision) => ManageCollisions(collision);
+        
         //? WORK IN PROGRESS
-        private void ManageCollisions(Collision2D collision)
-        {
-            contactNormal = Vector2.zero;
+         private void ManageCollisions(Collision2D collision)
+         {
+             timeSinceContact = 0;
+             contactNormal = Vector2.zero;
+             contacts = collision.contactCount;
+        
+             foreach (var contact in collision.contacts) contactNormal += contact.normal;
 
-            foreach (var contact in collision.contacts)
-            {
-                contactNormal += contact.normal;
-            }
-            
-            contactNormal.Normalize();
-
-            onWall = (Vector2.Dot(contactNormal, Vector2.up) > -0.1f);
-
-            Debug.DrawRay(rigidbody.position, contactNormal, Color.magenta);
-        }
+             contactNormal.Normalize();
+        
+             onWall = (contactNormal == Vector2.left || contactNormal == Vector2.right);
+        
+             Debug.DrawRay(rigidbody.position, contactNormal, Color.magenta);
+         }
 
 
         //> DRAW HELPFUL GIZMOS
@@ -118,7 +132,7 @@ namespace OGAM.Player
             if (!Application.isPlaying) return;
 
             Gizmos.color = (grounded) ? Color.green : Color.red;
-            Gizmos.DrawRay(rigidbody.position, Vector3.down * GroundedDistance);
+            Gizmos.DrawRay(rigidbody.position, Vector3.down * groundedDistance);
         }
     }
 }
