35a5b853d7c11aab64d3f11ef1a7d60cf9be971f
Postal-Servis <jason1228@mailfence.com>
2021-07-11 13:01:15 -0500

    Moved a lot of stuff to the new Scene Management folder
    Also added XML Manager script, following a save/load game tutorial

---
+++ Assets/Scripts/Scene Management/XMLManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OGAM
+{
+    public class XMLManager : MonoBehaviour
+    {   
+        
+
+        void Awake()
+        {
+            
+        }
+    }
+}
