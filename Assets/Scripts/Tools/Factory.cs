fbc8fb34f01e8a271506505345571a12595374ee
tcm <tylermckay10@gmail.com>
2021-07-13 01:44:32 -0300

    added Factory spawner

---
+++ Assets/Scripts/Tools/Factory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+namespace OGAM.Tools
+{
+    public class Factory : ScriptableObject
+    {
+        public static T Spawn<T>(T prefab, Vector3 position) where T : MonoBehaviour
+            => Instantiate(prefab, position, Quaternion.identity);
+    }
+}
