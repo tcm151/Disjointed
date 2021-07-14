6336cd782b3fbc1e7a110752704dc42c1a82c15e
tcm <tylermckay10@gmail.com>
2021-07-14 13:20:17 -0300

    added some additional factory spawn methods

--- Assets/Scripts/Tools/Factory.cs
+++ Assets/Scripts/Tools/Factory.cs
@@ -5,7 +5,13 @@ namespace OGAM.Tools
 {
     public class Factory : ScriptableObject
     {
+        public static T Spawn<T>(T prefab, Vector3 position, Quaternion rotation) where T : MonoBehaviour
+            => Instantiate(prefab, position, rotation);
+
         public static T Spawn<T>(T prefab, Vector3 position) where T : MonoBehaviour
-            => Instantiate(prefab, position, Quaternion.identity);
+            => Spawn(prefab, position, Quaternion.identity);
+        
+        public static T Spawn<T>(T prefab) where T : MonoBehaviour
+            => Spawn(prefab, Vector3.zero, Quaternion.identity);
     }
 }
