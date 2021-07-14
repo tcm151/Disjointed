f6d4d3eb841b54cfc2c914a1404a85d2e2f749e0
tcm <tylermckay10@gmail.com>
2021-07-14 13:20:51 -0300

    reworking projectile for gun

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -19,30 +19,46 @@ namespace OGAM.Combat
         }
         
         public Data data;
+        
         new private Rigidbody2D rigidbody;
+        // private Vector3 previousPosition; 
 
         //> INITIALIZATION
         private void Awake()
         {
             rigidbody = GetComponent<Rigidbody2D>();
-            rigidbody.mass = data.mass;
+            // rigidbody.mass = data.mass;
         }
 
         //> FIRE WITH A GIVEN VELOCITY
         public void Launch(Vector3 position, Vector3 direction, float speed, Data data)
         {
             this.data = data;
-            rigidbody.mass = data.mass;
+            // rigidbody.mass = data.mass;
             rigidbody.position = position;
-            rigidbody.AddForce(direction * (speed * data.mass), ForceMode2D.Impulse);
+            rigidbody.AddForce(direction * (speed * rigidbody.mass), ForceMode2D.Impulse);
         }
+        
+        // //> CHECK IMPACT FOR BALLISTIC PROJECTILES
+        // virtual protected void CheckImpact()
+        // {
+        //     if (Physics.Linecast(previousPosition, rigidbody.position, out RaycastHit hit))
+        //     {
+        //         IDamageable damageable = hit.collider.GetComponent<IDamageable>();
+        //         damageable?.TakeDamage(data.damage, data.origin);
+        //         damageable?.TakeKnockback(data.knockback, rigidbody.velocity.normalized);
+        //
+        //         Destroy(this.gameObject);
+        //     }
+        //     else previousPosition = rigidbody.position;
+        // }
 
         //> DO DAMAGE ON COLLISION
         private void OnCollisionEnter2D(Collision2D collision)
         {
             var damageable = collision.gameObject.GetComponent<IDamageable>();
             damageable?.TakeDamage(data.damage, data.origin);
-            // damageable?.TakeKnockback(data.knockback * data.mass, rigidbody.velocity.normalized);
+            damageable?.TakeKnockback(data.knockback, rigidbody.velocity.normalized);
         }
     }
 }
