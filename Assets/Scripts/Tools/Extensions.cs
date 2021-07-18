08e8917e183bdb1c91d4e198bfdeb582ebe593cb
tcm <tylermckay10@gmail.com>
2021-07-18 12:44:29 -0300

    undid upgrade lol

--- Assets/Scripts/Tools/Extensions.cs
+++ Assets/Scripts/Tools/Extensions.cs
@@ -27,8 +27,8 @@ namespace OGAM.Tools
         }
         
         //> MATHF
-        public static float Clamp(this float value, float min = float.MinValue, float max = float.MaxValue)
-            => Mathf.Clamp(value, min, max);
+        public static float Clamp(this ref float value, float min, float max)
+            => value = Mathf.Clamp(value, min, max);
 
 
 
