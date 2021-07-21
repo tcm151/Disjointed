c4fc5ebb1b109a894a17dd2b7d5f418238e52246
tcm <tylermckay10@gmail.com>
2021-07-20 23:07:11 -0300

    attack animation & sprite animation script

---
+++ Assets/Scripts/Sprites/SpriteAnimation.cs
@@ -0,0 +1,31 @@
+﻿#pragma warning disable 108,114
+
+using UnityEngine;
+
+
+namespace Disjointed.Sprites
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteAnimation : MonoBehaviour
+    {
+        private Animator animator;
+        private SpriteRenderer renderer;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            renderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void PlayAnimation(string animation)
+        {
+            animator.Play(animation);
+        }
+
+        public void TriggerAnimation(string name)
+        {
+            animator.SetTrigger(name);
+        }
+    }
+}
