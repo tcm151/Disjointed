cd77f3923bcaa2830f2b76de95da234421089bd3
tcm <tylermckay10@gmail.com>
2021-07-27 17:07:23 -0300

    added a game manager class

---
+++ Assets/Scripts/Game Management/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Disjointed.Tools.SceneManagement;
+using Disjointed.UI;
+using UnityEngine;
+
+
+namespace Disjointed.Game_Management
+{
+    public class GameManager : ScriptableObject
+    {
+        public static bool IsPaused => (Time.timeScale == 0f);
+        public static bool IsPlaying => (Time.timeScale > 0f);
+        
+        public static void Pause() => Time.timeScale = 0f;
+        public static void Resume() => Time.timeScale = 1f;
+        
+        public static void Restart()
+        {
+            SceneSwitcher.ReloadScene();
+            Resume();
+        }
+        
+        //> QUIT THE GAME
+        public static void Quit() => SceneSwitcher.QuitGame();
+    }
+}
