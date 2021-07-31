9086a275b0c26bb0f1bc609de0476e1b36e3e207
tcm <tylermckay10@gmail.com>
2021-07-31 17:08:16 -0300

    hooking up rats

---
+++ Assets/Scripts/Combat/Enemies/Bat.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace Disjointed.Combat.Enemies
+{
+    public class Bat : Enemy
+    {
+        override protected void Update()
+        {
+            base.Update();
+
+            SetAnimationState("Flying", IsMoving);
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/Enemies/Rat.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace Disjointed.Combat.Enemies
+{
+    public class Rat : Enemy
+    {
+        override protected void Update()
+        {
+            base.Update();
+
+            SetAnimationState("Walking", IsMoving);
+        }
+    }
+}
