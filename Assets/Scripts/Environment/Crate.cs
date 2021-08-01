e77c7d0f81b4ffe19a3c6d3418cbd4d3ac139ff1
tcm <tylermckay10@gmail.com>
2021-08-01 16:21:56 -0300

    wooden crate

---
+++ Assets/Scripts/Environment/Crate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Disjointed.Combat;
+using UnityEngine;
+
+
+namespace Disjointed.Environment
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Crate : MonoBehaviour, IDamageable
+    {
+        new private Rigidbody2D rigidbody;
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void TakeDamage(float damage, string origin)
+        {
+            // naww maybe break later
+        }
+
+        public void TakeKnockback(Vector2 direction, float knockback)
+        {
+            rigidbody.AddForce(direction * knockback, ForceMode2D.Impulse);
+        }
+    }
+}
