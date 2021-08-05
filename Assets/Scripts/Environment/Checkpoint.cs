842fc11b3133aa7aa6f770af549a2315e6b02fb1
tcm <tylermckay10@gmail.com>
2021-08-04 23:24:56 -0300

    cleaned up checkpoint code

--- Assets/Scripts/Environment/Checkpoint.cs
+++ Assets/Scripts/Environment/Checkpoint.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using Disjointed.Player;
 using Disjointed.Tools.Extensions;
 using Disjointed.Tools.Serialization;
 
@@ -11,20 +10,12 @@ namespace Disjointed.Environment
         public Serializer serializer;
         public LayerMask playerMask;
 
-        public Vector3 position => transform.position;
-        
         private void OnTriggerEnter2D(Collider2D collider)
         {
             // ignore anything except the thePlayer
             if (!playerMask.Contains(collider.gameObject.layer)) return;
 
             serializer.SaveGame();
-            Debug.Log("Saving Game!");
-            
-            var checkpointManager = collider.GetComponent<ThePlayer>();
-            if (!checkpointManager) return;
-            
-            checkpointManager.SetCheckpoint(this);
         }
     }
 }
