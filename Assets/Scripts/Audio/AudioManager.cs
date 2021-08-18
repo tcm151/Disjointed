bf20deffc695f6fc8169fb33e086a24c1d60c0bf
tcm <tylermckay10@gmail.com>
2021-08-18 15:56:25 -0300

    improved the music sequencing, added playlist function

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,19 +1,30 @@
 ﻿using System;
-using System.Collections;
 using System.Linq;
+using System.Collections;
 using UnityEngine;
+using Disjointed.Tools.Extensions;
 
 
 namespace Disjointed.Audio
 {
+    
     public class AudioManager : MonoBehaviour
+    //>---------------------------------------------------------------------------------------------
+    //> |    PURPOSE   |  
+    //>---------------------------------------------------------------------------------------------
+    //> |    AUTHORS   |
+    //> |   REFACTORS  |
+    //> | CONTRIBUTORS |
+    //>---------------------------------------------------------------------------------------------
     {
         //- LOCAL VARIABLES
         [SerializeField] private SFX[] soundEffects;
         [SerializeField] private AudioSource[] sources;
 
+        //- EVENT TRIGGERS
         public static Action<string> PlaySFX;
-        public static Action<string, int, bool> PlayTrack;
+        public static Action<SFX, int, bool> PlayTrack;
+        public static Action<string, int, bool> FindAndPlayTrack;
 
         //> INITIALIZATION
         private void Awake()
@@ -22,26 +33,40 @@ namespace Disjointed.Audio
 
             PlaySFX += OnPlaySFX;
             PlayTrack += OnPlayTrack;
-            
-            StartCoroutine(CR_MusicPlaylist());
+            FindAndPlayTrack += OnPlayTrack;
+
+            StartCoroutine(CR_StartMusic(true));
         }
 
-        private IEnumerator CR_MusicPlaylist()
+        //> MUSIC SEQUENCER
+        private IEnumerator CR_StartMusic(bool loop)
         {
-            OnPlayTrack("CaveBackgroundNoise", 0, true);
-            yield return new WaitForSeconds(soundEffects.First(s => s.name == "CaveBackgroundNoise").clip.length);
-            OnPlayTrack("CaveTheme1", 1);
-            yield return new WaitForSeconds(1f);
-            sources[0].volume = 0.5f;
-            yield return new WaitForSeconds(soundEffects.First(s => s.name == "CaveTheme1").clip.length);
-            sources[0].volume = 1f;
-            yield return new WaitForSeconds(soundEffects.First(s => s.name == "CaveBackgroundNoise").clip.length);
-            OnPlayTrack("CaveTheme2", 1);
-            yield return new WaitForSeconds(1f);
-            sources[0].volume = 0.5f;
-            yield return new WaitForSeconds(soundEffects.First(s => s.name == "CaveTheme2").clip.length);
+            var backgroundNoise = soundEffects.First(s => s.name == "CaveBackgroundNoise");
+            var caveTheme1 = soundEffects.First(s => s.name == "CaveTheme1");
+            var caveTheme2 = soundEffects.First(s => s.name == "CaveTheme2");
 
-            StartCoroutine(CR_MusicPlaylist());
+            OnPlayTrack(backgroundNoise, 0);
+            yield return new WaitForSeconds(backgroundNoise.clip.length);
+            OnPlayTrack(caveTheme1, 1);
+            yield return new WaitForSeconds(caveTheme1.clip.length);
+            OnPlayTrack(backgroundNoise, 0);
+            yield return new WaitForSeconds(backgroundNoise.clip.length);
+            OnPlayTrack(caveTheme2, 1);
+            yield return new WaitForSeconds(caveTheme2.clip.length);
+            
+            if (loop) StartCoroutine(CR_StartMusic(loop));
+        }
+
+        private IEnumerator CR_PlayPlaylist(SFX[] tracks, int channel, bool shuffle, bool loop)
+        {
+            if (tracks.Length <= 0) Debug.LogWarning("Provided <colors=yellow>NO SONGS</color> in Playlist!");
+            if (shuffle) tracks = tracks.Shuffle().ToArray();
+            
+            foreach (var track in tracks)
+            {
+                OnPlayTrack(track, channel);
+                yield return new WaitForSeconds(track.clip.length);
+            }
         }
 
         //> PLAY ONE SHOT SOUND CLIP
@@ -49,23 +74,29 @@ namespace Disjointed.Audio
         {
             SFX sfx = soundEffects.FirstOrDefault(s => s.name == name);
             if (sfx is { }) sources[1].PlayOneShot(sfx.clip, sfx.volume);
-            else Debug.LogWarning($"Unable to find sound: <color=yellow>{name}</color>");
+            else Debug.LogWarning($"Unable to find sfx: <color=yellow>\"{name}\"</color>");
         }
 
         //> REPLACE STREAM SOUND CLIP
-        private void OnPlayTrack(string sound, int track, bool loop = false)
+        private void OnPlayTrack(string track, int channel , bool loop = false)
         {
-            SFX sfx = soundEffects.FirstOrDefault(s => s.name == sound);
+            SFX sfx = soundEffects.FirstOrDefault(s => s.name == track);
+            OnPlayTrack(sfx, channel, loop);
+        }
+
+        private void OnPlayTrack(SFX sfx, int channel, bool loop = false)
+        {
             if (sfx is { })
             {
-                sources[track].clip = sfx.clip;
-                sources[track].pitch = sfx.pitch;
-                sources[track].volume = sfx.volume;
-                sources[track].loop = loop;
-                sources[track].Play();
+                sources[channel].clip = sfx.clip;
+                sources[channel].pitch = sfx.pitch;
+                sources[channel].volume = sfx.volume;
+                sources[channel].loop = loop;
+                sources[channel].Play();
             }
-            else Debug.LogWarning($"Unable to find sound: <color=yellow>{name}</color>");
+            else Debug.LogWarning($"Unable to find track: <color=yellow>\"{name}\"</color>");
         }
+
 
         //> STOP STREAM SOUND CLIP
         private void Stop(int stream) => sources[stream].Stop();
